cb93bfd43626c5589b7ba7c33650f284906129d9
Tristan Protzman <tlprotzman+github@gmail.com>
2016-10-08 15:36:09 -0400

    port game.py

---
+++ src/game.py
@@ -0,0 +1,43 @@
+import pygame
+
+class Game:
+	def __init__(self):
+		self.clock = pygame.time.Clock()
+		self.currentLevel = 0
+		self.gameState = 'STARTSCREEN'
+		self.levelList = []
+		self.levelCounter = 0
+		self.enemyList = []
+		self.nameList = []
+		self.life = 3
+		self.animation = 0
+		self.animationCounter = 0
+
+
+	def setCurrentLevel(self, newLevel):
+		self.currentLevel = newLevel
+
+	def getCurrentlevel(self):
+		return self.currentLevel
+
+	def animate(self):
+		self.animationCounter += 1
+		if self.animationCounter == 7:
+			self.animation += 1
+			if self.animation == 4:
+				self.animation = 0
+			self.animationCounter = 0
+
+	def setGameState(self, newGameState):
+		self.gameState = newGameState
+
+	def getGameState(self):
+		return self.gameState
+
+	def setTileSet(self):
+	#Fill in with the images that are done
+
+	def drawLives(self):
+		for i in range(game.life):
+			window.blit(game.lifeImage, (20 + i * 60, 64))		#Make sure to set the life image!!!
+
