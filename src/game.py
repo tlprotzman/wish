acf73ff38fc29d186e9841c6dadf1bf7864c7a4d
Jordan <jmanfb@gmail.com>
2016-10-09 02:27:39 -0400

    changed genie text size

--- src/game.py
+++ src/game.py
@@ -21,7 +21,7 @@ class Game:
 		self.camera_y = 0
 		self.close_parallax_scale = .25
 		self.far_parallax_scale = .1
-		self.genieFont = pygame.font.Font("../fonts/joystixMonospace.ttf", 16)
+		self.genieFont = pygame.font.Font("../fonts/joystixMonospace.ttf", 20)
 
 
 
