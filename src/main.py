e8004376523bdd961eb7a12844060b432a088236
Jordan <jmanfb@gmail.com>
2016-10-08 20:29:19 -0400

    camera now stops at level edges

--- src/main.py
+++ src/main.py
@@ -56,14 +56,15 @@ def main():
 			player.update(game.camera_x, game.camera_y)
 			game.getCurrentLevel().update(game.camera_x, game.camera_y)
 			
-			if player.rect.x + player.rect.width / 2 < 704:
+
+			game.camera_x += ((player.rect.x+player.rect.width/2-game.screenWidth/2) - game.camera_x)/5
+			game.camera_y = 0
+
+			if game.camera_x < 0:
 				game.camera_x = 0
-				game.camera_y = 0
-			elif player.rect.x + player.rect.width / 2 > game.getCurrentLevel().getLevelWidth():
-				game.camera_x - game.getCurrentLevel().getLevelWidth() - screenWidth
-			else:
-				game.camera_x += ((player.rect.x+player.rect.width/2-game.screenWidth/2) - game.camera_x)/5
-				game.camera_y = 0
+			if game.camera_x > game.getCurrentLevel().getLevelWidth()-game.screenWidth:
+				game.camera_x = game.getCurrentLevel().getLevelWidth()-game.screenWidth
+			
 			print(player.rect.x+player.rect.width/2)
 			# game.levelTitle.update()
 			# game.drawLives()
